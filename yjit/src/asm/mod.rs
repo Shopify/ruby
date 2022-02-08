ee34fe335c1795672937f70b4eabf5b1e8d19d97
Kevin Newton <kddnewton@gmail.com>
2022-02-08 13:50:23 -0500

    Labels and jumps assembly

--- yjit/src/asm/mod.rs
+++ yjit/src/asm/mod.rs
@@ -10,6 +10,11 @@ impl CodePtr {
         let CodePtr(ptr) = self;
         return ptr;
     }
+
+    fn into_i64(&self) -> i64 {
+        let CodePtr(ptr) = self;
+        *ptr as i64
+    }
 }
 
 impl From<*mut u8> for CodePtr {
