b17493485f171978719bb2654e7254888628a0f5
Peter Zhu <peter@peterzhu.ca>
2022-04-13 17:05:24 -0400

    Fix incorrect link in docs (#244)

--- lib/csv/row.rb
+++ lib/csv/row.rb
@@ -570,7 +570,7 @@ class CSV
     # by +index_or_header+ and +specifiers+.
     #
     # The nested objects may be instances of various classes.
-    # See {Dig Methods}[rdoc-ref:dig_methods.rdoc].
+    # See {Dig Methods}[https://docs.ruby-lang.org/en/master/dig_methods_rdoc.html].
     #
     # Examples:
     #   source = "Name,Value\nfoo,0\nbar,1\nbaz,2\n"
