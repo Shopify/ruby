5b8f2409a273ac45de6b1a3e494435bcd0a377a3
Alan Wu <XrXr@users.noreply.github.com>
2022-04-25 19:06:55 -0400

    WIP clippy and rustfmt

--- yjit/src/lib.rs
+++ yjit/src/lib.rs
@@ -3,6 +3,7 @@
 #![allow(dead_code)]
 #![allow(unused_assignments)]
 #![allow(unused_macros)]
+#![allow(clippy::style)] // We are laid back about style
 
 mod asm;
 mod cruby;
@@ -12,13 +13,10 @@ mod invariants;
 mod disasm;
 mod utils;
 mod options;
-mod stats;
 mod yjit;
 
-#[cfg(test)]
-mod tests {
-    /// Just as a sandbox for playing around
-    #[test]
-    fn sandbox() {
-    }
-}
+// A bit unfortunate. Can't use `#![rustfmt::skip::macros(make_counters)]`
+// because it's [unstable](https://github.com/rust-lang/rust/issues/54726).
+// Also, rustfmt doesn't seem to pick up `#[rustfmt...]`, the outer attribute syntax.
+#[rustfmt::skip]
+mod stats;
