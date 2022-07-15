d160253b0eb8ee74e0ee52e17fbb044c9935856a
Kevin Newton <kddnewton@gmail.com>
2022-07-15 10:23:35 -0400

    Port print_str to the new backend

--- yjit/src/utils.rs
+++ yjit/src/utils.rs
@@ -1,7 +1,6 @@
 #![allow(dead_code)] // Some functions for print debugging in here
 
-use crate::asm::x86_64::*;
-use crate::asm::*;
+use crate::backend::ir::*;
 use crate::cruby::*;
 use std::slice;
 
@@ -129,34 +128,7 @@ macro_rules! c_callable {
 }
 pub(crate) use c_callable;
 
-// Save caller-save registers on the stack before a C call
-fn push_regs(cb: &mut CodeBlock) {
-    push(cb, RAX);
-    push(cb, RCX);
-    push(cb, RDX);
-    push(cb, RSI);
-    push(cb, RDI);
-    push(cb, R8);
-    push(cb, R9);
-    push(cb, R10);
-    push(cb, R11);
-    pushfq(cb);
-}
-
-// Restore caller-save registers from the after a C call
-fn pop_regs(cb: &mut CodeBlock) {
-    popfq(cb);
-    pop(cb, R11);
-    pop(cb, R10);
-    pop(cb, R9);
-    pop(cb, R8);
-    pop(cb, RDI);
-    pop(cb, RSI);
-    pop(cb, RDX);
-    pop(cb, RCX);
-    pop(cb, RAX);
-}
-
+/*
 pub fn print_int(cb: &mut CodeBlock, opnd: X86Opnd) {
     c_callable!{
         fn print_int_fn(val: i64) {
@@ -185,7 +157,9 @@ pub fn print_int(cb: &mut CodeBlock, opnd: X86Opnd) {
     call(cb, RAX);
     pop_regs(cb);
 }
+*/
 
+/*
 /// Generate code to print a pointer
 pub fn print_ptr(cb: &mut CodeBlock, opnd: X86Opnd) {
     c_callable!{
@@ -202,7 +176,9 @@ pub fn print_ptr(cb: &mut CodeBlock, opnd: X86Opnd) {
     call(cb, RAX);
     pop_regs(cb);
 }
+*/
 
+/*
 /// Generate code to print a value
 pub fn print_value(cb: &mut CodeBlock, opnd: X86Opnd) {
     c_callable!{
@@ -221,9 +197,10 @@ pub fn print_value(cb: &mut CodeBlock, opnd: X86Opnd) {
 
     pop_regs(cb);
 }
+*/
 
 /// Generate code to print constant string to stdout
-pub fn print_str(cb: &mut CodeBlock, str: &str) {
+pub fn print_str(asm: &mut Assembler, str: &str) {
     c_callable!{
         fn print_str_cfun(ptr: *const u8, num_bytes: usize) {
             unsafe {
@@ -234,26 +211,18 @@ pub fn print_str(cb: &mut CodeBlock, str: &str) {
         }
     }
 
-    let bytes = str.as_ptr();
-    let num_bytes = str.len();
+    asm.cpush_all();
 
-    push_regs(cb);
+    let string_data = asm.new_label("string_data");
+    let after_string = asm.new_label("after_string");
 
-    // Load the string address and jump over the string data
-    lea(cb, C_ARG_REGS[0], mem_opnd(8, RIP, 5));
-    jmp32(cb, num_bytes as i32);
+    asm.jmp(after_string);
+    asm.write_label(string_data);
+    asm.bake_string(str);
+    asm.write_label(after_string);
 
-    // Write the string chars and a null terminator
-    for i in 0..num_bytes {
-        cb.write_byte(unsafe { *bytes.add(i) });
-    }
+    let opnd = asm.lea_label(string_data);
+    asm.ccall(print_str_cfun as *const u8, vec![opnd, Opnd::UImm(str.len() as u64)]);
 
-    // Pass the string length as an argument
-    mov(cb, C_ARG_REGS[1], uimm_opnd(num_bytes as u64));
-
-    // Call the print function
-    mov(cb, RAX, const_ptr_opnd(print_str_cfun as *const u8));
-    call(cb, RAX);
-
-    pop_regs(cb);
+    asm.cpop_all();
 }
