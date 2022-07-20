4fc044e849fadc3d227ceb843bd23fcc713ba439
Noah Gibbs <the.codefolio.guy@gmail.com>
2022-07-20 15:54:39 +0100

    Fix dupn (#330)
    * get_dupn was allocating and throwing away an Assembler object instead of using the one passed in
    
    * Uncomment remaining tests in codegen.rs, which seem to work now

--- yjit/src/codegen.rs
+++ yjit/src/codegen.rs
@@ -934,8 +934,6 @@ fn gen_dupn(
     _ocb: &mut OutlinedCb,
 ) -> CodegenStatus {
 
-    let mut asm = Assembler::new();
-
     let nval: VALUE = jit_get_arg(jit, 0);
     let VALUE(n) = nval;
 
@@ -6318,7 +6316,6 @@ mod tests {
         assert!(ocb.unwrap().get_write_pos() > 0);
     }
 
-    /*
     #[test]
     fn test_gen_exit() {
         let (_, ctx, mut asm, mut cb, _) = setup_codegen();
@@ -6333,7 +6330,6 @@ mod tests {
          get_side_exit(&mut jit, &mut ocb, &ctx);
         assert!(ocb.unwrap().get_write_pos() > 0);
     }
-    */
 
     #[test]
     fn test_gen_check_ints() {
@@ -6379,7 +6375,6 @@ mod tests {
         assert!(cb.get_write_pos() > 0); // Write some movs
     }
 
-    /*
     #[test]
     fn test_gen_dupn() {
         let (mut jit, mut context, mut asm, mut cb, mut ocb) = setup_codegen();
@@ -6403,7 +6398,6 @@ mod tests {
         asm.compile(&mut cb);
         assert!(cb.get_write_pos() > 0); // Write some movs
     }
-    */
 
     #[test]
     fn test_gen_swap() {
