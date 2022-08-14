1ad28d1de2b1c00a5f6f0e0d38f741907a9cefa8
wildmaples <maple.develops@gmail.com>
2022-08-14 10:57:17 -0400

    Use new asm backend

--- yjit/src/codegen.rs
+++ yjit/src/codegen.rs
@@ -1260,24 +1260,22 @@ fn gen_splatarray(
 fn gen_concatarray(
     jit: &mut JITState,
     ctx: &mut Context,
-    cb: &mut CodeBlock,
+    asm: &mut Assembler,
     _ocb: &mut OutlinedCb,
 ) -> CodegenStatus {
     // Save the PC and SP because the callee may allocate
     // Note that this modifies REG_SP, which is why we do it first
-    jit_prepare_routine_call(jit, ctx, cb, REG0);
+    jit_prepare_routine_call(jit, ctx, asm);
 
     // Get the operands from the stack
     let ary2st_opnd = ctx.stack_pop(1);
     let ary1_opnd = ctx.stack_pop(1);
 
     // Call rb_vm_concat_array(ary1, ary2st)
-    mov(cb, C_ARG_REGS[0], ary1_opnd);
-    mov(cb, C_ARG_REGS[1], ary2st_opnd);
-    call_ptr(cb, REG1, rb_vm_concat_array as *const u8);
+    let ary = asm.ccall(rb_vm_concat_array as *const u8, vec![ary1_opnd, ary2st_opnd]);
 
     let stack_ret = ctx.stack_push(Type::Array);
-    mov(cb, stack_ret, RAX);
+    asm.mov(stack_ret, ary);
 
     KeepCompiling
 }
