b821bdae8fe231cea10f494214a82579a1aa2a7a
Maple Ong <maple.develops@gmail.com>
2022-08-15 12:54:26 -0400

    YJIT: Implement concatarray in yjit (#405)
    * Create code generation func
    
    * Make rb_vm_concat_array available to use in Rust
    
    * Map opcode to code gen func
    
    * Implement code gen for concatarray
    
    * Add test for concatarray
    
    * Use new asm backend
    
    * Add comment to C func wrapper

--- vm_insnhelper.c
+++ vm_insnhelper.c
@@ -4379,6 +4379,14 @@ vm_concat_array(VALUE ary1, VALUE ary2st)
     return rb_ary_concat(tmp1, tmp2);
 }
 
+// YJIT implementation is using the C function
+// and needs to call a non-static function
+VALUE
+rb_vm_concat_array(VALUE ary1, VALUE ary2st)
+{
+    return vm_concat_array(ary1, ary2st);
+}
+
 static VALUE
 vm_splat_array(VALUE flag, VALUE ary)
 {
@@ -4394,6 +4402,8 @@ vm_splat_array(VALUE flag, VALUE ary)
     }
 }
 
+// YJIT implementation is using the C function
+// and needs to call a non-static function
 VALUE
 rb_vm_splat_array(VALUE flag, VALUE ary)
 {
