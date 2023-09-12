9c3a320c877c2b5e4761b4ac395c1bfaa712e92f
Maxime Chevalier-Boisvert <maxime.chevalierboisvert@shopify.com>
2023-09-12 16:53:55 -0400

    Add new counters to DEFAULT_COUNTERS

--- yjit/src/stats.rs
+++ yjit/src/stats.rs
@@ -143,13 +143,15 @@ macro_rules! make_counters {
 
 /// The list of counters that are available without --yjit-stats.
 /// They are incremented only by `incr_counter!` and don't use `gen_counter_incr`.
-pub const DEFAULT_COUNTERS: [&str; 6] = [
+pub const DEFAULT_COUNTERS: [&str; 8] = [
     "code_gc_count",
     "compiled_iseq_entry",
     "compiled_iseq_count",
     "compiled_blockid_count",
     "compiled_block_count",
     "compiled_branch_count",
+    "branch_stub_hit",
+    "branch_stub_hit_old",
 ];
 
 /// Macro to increase a counter by name and count
