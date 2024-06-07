2a18ec79b68ca6809335a736eb06fadbb6eba054
Aaron Patterson <tenderlove@ruby-lang.org>
2024-06-07 09:41:13 -0700

    Receivers with positional-only parameters should be cacheable

--- vm_insnhelper.h
+++ vm_insnhelper.h
@@ -271,7 +271,8 @@ vm_call_cacheable(const struct rb_callinfo *ci, const struct rb_callcache *cc)
 static inline bool
 vm_call_iseq_optimizable_p(const struct rb_callinfo *ci, const struct rb_callcache *cc)
 {
-    return !IS_ARGS_SPLAT(ci) && !IS_ARGS_KEYWORD(ci) && vm_call_cacheable(ci, cc);
+    return !IS_ARGS_SPLAT(ci) && !IS_ARGS_KEYWORD(ci) && ((vm_ci_flag(ci) & VM_CALL_FCALL) ||
+        METHOD_ENTRY_VISI(vm_cc_cme(cc)) != METHOD_VISI_PROTECTED);
 }
 
 #endif /* RUBY_INSNHELPER_H */
