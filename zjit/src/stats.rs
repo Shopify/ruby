f95f579a5f6d58b6e0e2a7bbda48acacd95559f2
Jacob Denbeaux <jacob.denbeaux@shopify.com>
2026-03-23 17:24:29 -0400

    Add dead store optimization, tests, and counter

--- zjit/src/stats.rs
+++ zjit/src/stats.rs
@@ -169,6 +169,7 @@ make_counters! {
         compile_hir_build_time_ns,
         compile_hir_strength_reduce_time_ns,
         compile_hir_optimize_load_store_time_ns,
+        compile_hir_eliminate_dead_stores_time_ns,
         compile_hir_fold_constants_time_ns,
         compile_hir_clean_cfg_time_ns,
         compile_hir_remove_redundant_patch_points_time_ns,
