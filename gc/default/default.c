6cf032e238b132639381c57ded41af7d661288cd
Luke Gruber <luke.gruber@shopify.com>
2026-04-02 00:30:49 -0400

    Remove old rb_bug

--- gc/default/default.c
+++ gc/default/default.c
@@ -5582,9 +5582,6 @@ gc_sweep_step(rb_objspace_t *objspace, rb_heap_t *heap)
 #endif
 
         if (free_slots == sweep_page->total_slots) {
-            if (sweep_page->total_slots == 0) {
-                rb_bug("?");
-            }
 #if RUBY_DEBUG
             objspace->have_swept_slots += free_slots;
 #endif
